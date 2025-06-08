73346479519b5bedac723c4ff0355e414018b013
MisaqArtist7 <misaqbabakhani777@gmail.com>
2025-06-08 17:05:30 +0330

    fix bug

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -38,7 +38,7 @@ const signUpSchema = z
   type signUpFormInputs = z.infer<typeof signUpSchema>;
 
   // Initialize react-hook-form with Zod resolver for validation
-  const { register, handleSubmit, formState: { errors } } = useForm<signUpFormInputs>({ resolver: zodResolver(signUpSchema) });
+  const { register, handleSubmit, setError, formState: { errors } } = useForm<signUpFormInputs>({ resolver: zodResolver(signUpSchema) });
 
   // Watch the agreeTerm checkbox to enable/disable submit button
   // const isAgreeTermeChecked = watch('agreeTerm');
@@ -100,6 +100,10 @@ const signUpSchema = z
     .catch((error) => {
       if (error.response && error.response.status === 422) {
         console.log('❌ Validation Error Payload:', error.response.data);
+        setError("email", {
+          type: "server",
+          message: error.response.data.errors.email,
+        });
       } else {
         console.error('❌ Other Error:', error);
       }
