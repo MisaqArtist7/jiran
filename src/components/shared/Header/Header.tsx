2ad4d23c1e2a28c20a322c434c82e599c4a5b6a5
hosseinGith <hosseinderees7@gmail.com>
2025-09-02 14:52:43 +0330

    fixed links tag

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -40,16 +40,16 @@ export default function Header() {
               </Link>
             </li>
             <li className="header__menu-item">
-              <a href="#" className="flex-row-center gap-x-1">
+              <Link href="#" className="flex-row-center gap-x-1">
                 <BriefcaseIcon className="w-5 h-5" />
                 Business
-              </a>
+              </Link>
             </li>
             <li className="header__menu-item">
-              <a href="#" className="flex-row-center gap-x-1">
+              <Link href="#" className="flex-row-center gap-x-1">
                 <NewspaperIcon className="w-5 h-5" />
                 Blogs
-              </a>
+              </Link>
             </li>
             <li className="header__menu-item">
               <Link href="./login" className="flex-row-center gap-x-1">
@@ -61,13 +61,13 @@ export default function Header() {
         </div>
         {/* Icon section (e.g., profile, cart, etc.) */}
         {/* <div className="header__icons flex-row-center gap-x-2">
-          <a href="" className='relative bg-[var(--primaryColor)] rounded-full p-2.5 text-white'>
+          <Link href="" className='relative bg-[var(--primaryColor)] rounded-full p-2.5 text-white'>
             <BellAlertIcon className='w-6 h-6'/>
             <span className='absolute -top-1 -right-1 bg-[var(--primaryColor)] text-white rounded-full border border-white w-4 h-4 text-xs flex-row-center'>1</span>
-          </a>
-          <a href="./login" className='hover:bg-[var(--primaryColor)] bg-white border-2 border-[var(--primaryColor)] text-[var(--primaryColor)] rounded-full p-2.5 hover:text-white flex-row-center transition-colors duration-150'>
+          </Link>
+          <Link href="./login" className='hover:bg-[var(--primaryColor)] bg-white border-2 border-[var(--primaryColor)] text-[var(--primaryColor)] rounded-full p-2.5 hover:text-white flex-row-center transition-colors duration-150'>
             <UserIcon className='w-6 h-6'/>
-          </a>
+          </Link>
         </div> */}
       </nav>
     </header>
